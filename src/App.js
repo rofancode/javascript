208e664164b8c930441ba1740c3c6c65453d9caa
rofancode <rofancode@gamil.com>
2023-02-21 09:38:42 +0900

    기본강의 프로젝트 count

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
+function Counter({title, initValue}) {
+  useEffect(() => {
+    fetch('/counter')
+    .then(resp => resp.json)
+    .then(result => setCount(result.value))
+  },[])
+
+  const [count, setCount] = useState(initValue);
+
+  const up = () => {
+    const options = {
+      method : "PATCH",
+      header : {'Content-Type' : 'application/json'},
+      body : JSON.stringify({value : count + 1}),
+    }
+    fetch('/counter',options)
+    .then(resp => resp.json)
+    .then(result => setCount(result.value))
+  }
+  const down = () => setCount(prevCount => prevCount - 1);
+  const reset = () => setCount(0);
+
+  return <div> 
+    <h1> {title} </h1>
+    <button onClick={up}>+</button>
+    <button onClick={down}>-</button>
+    <button onClick={reset}>Reset</button>
+    👉 {count}
+  </div>
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter title ="카운트" initValue = {5}/>
     </div>
   );
 }
